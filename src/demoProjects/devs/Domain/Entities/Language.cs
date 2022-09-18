b0c347cf2618204d03c674ee29939bef4779e5fe
Tuncay Altinpulluk <tuncayalt@gmail.com>
2022-09-18 19:56:30 +0300

    Added technologies table to dbcontext

--- src/demoProjects/devs/Domain/Entities/Language.cs
+++ src/demoProjects/devs/Domain/Entities/Language.cs
@@ -5,6 +5,7 @@ namespace Domain.Entities
     public class Language : Entity
     {
         public string Name { get; set; }
+        public virtual ICollection<Technology> Technologies { get; set; }
 
         public Language()
         {
